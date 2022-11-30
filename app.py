dd536ad31be05b72ed1a54d5f43308b70deaf4aa
Humanshu D G <83282277+HumanshuDG@users.noreply.github.com>
2022-11-30 18:55:04 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+st.title("TDS Module 8")
+a = st.number_input("Enter first number: ")
+b = st.number_input("Enter second number: ") 
+c = a * b
+st.write("Product of given number is", c)
+st.write("© 21F1001893")
