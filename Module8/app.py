45a5f0aed3557300a699b9d54e379e726cd12130
Humanshu D G <83282277+HumanshuDG@users.noreply.github.com>
2022-12-06 19:07:33 +0530

    Update app.py

--- Module8/app.py
+++ Module8/app.py
@@ -4,4 +4,4 @@ a = st.number_input("Enter first number: ")
 b = st.number_input("Enter second number: ") 
 c = a * b
 st.write("Product of given number is", c)
-st.write("© 21F1001893")
+st.write("Done by: Humanshu D G \(21F1001893\)")
